29514629b02c2d1e28a85df023f6e1e1c59b4dd5
pigate <mimchiang@gmail.com>
2015-09-26 21:51:10 -0700

    added critical section to openmp.c

--- openmp_for.c
+++ openmp_for.c
@@ -6,20 +6,37 @@
 
 int main(int argc, char **argv)
 {
-  int i, thread_id, nloops;
- 
-  //thread_id and nloops become each thread's private variables 
-  #pragma omp parallel private(thread_id, nloops)
+  int i, thread_id;
+  int glob_nloops, priv_nloops;
+  glob_nloops = 0;
+
+  //parallelize this chunk of code 
+  //thread_id and priv_nloops become each thread's private variables 
+  #pragma omp parallel private(thread_id, priv_nloops)
   {
-    nloops = 0;
+    priv_nloops = 0;
     thread_id = omp_get_thread_num();
+
+   //parallelize this for loop (every thread gets a chunk)
    #pragma omp for
     for(i = 0; i < 1000000; ++i)
     {
-      ++nloops; 
+      ++priv_nloops; 
     }
     printf("Thread %d performed %d iterations of the loop.\n",
-	thread_id, nloops);   
+	thread_id, priv_nloops);   
+    //"critical" code section for updating a global variable
+    //only one thread can enter at a time
+
+    #pragma omp critical
+    {
+      printf("Thread %d is adding its iterations (%d) to sum (%d), ",
+      	thread_id, priv_nloops, glob_nloops);
+      glob_nloops += priv_nloops;
+      printf("total loops is now %d.\n", glob_nloops);
+    } 
   } 
+  printf("Total # loop iterations is %d\n",
+      glob_nloops);
   return 0;
 }
